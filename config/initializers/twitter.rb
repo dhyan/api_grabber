19036f08189f23331674775b0807124a06847700
dhyan <dhyanbaba@gmail.com>
2015-01-25 00:02:08 +0530

    twitter tokens comment

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,8 @@
+
+#Enter the keys..Sign-up and fins it from aps.twitter.com
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "PTLCU3Oxz61AeD1nZh7qGSXPj"
-  config.consumer_secret     = "XYZkMErRJ1X85TXABNnsmKxTnjTGa1uBAFLhbRbXpOrwaA9acP"
-  config.access_token        = "2512972026-Fxvvt6jo7d77a2igrJahNjxWGFqoU3H9vYbBi4A"
-  config.access_token_secret = "qkufa46zu8GIfdklrc2GmGVlo9237qWJbIXsV90Zv3vFD"
+  config.consumer_key        = "XXXXXXXXXXXXXXXXXXX"
+  config.consumer_secret     = "XXXXXXXXXXXXXXXXXXX"
+  config.access_token        = "XXXXXXXXXXXXXXXXXXX"
+  config.access_token_secret = "XXXXXXXXXXXXXXXXXXX"
 end
