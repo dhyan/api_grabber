3b9019b93d6132a7d361a4f11ba28b1f7a5fbe0e
dhyan <dhyanbaba@gmail.com>
2015-03-22 00:23:24 +0530

    temp update for twitter token

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,8 +1,18 @@
 
 #Enter the keys..Sign-up and fins it from aps.twitter.com
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "XXXXXXXXXXXXXXXXXXX"
-  config.consumer_secret     = "XXXXXXXXXXXXXXXXXXX"
-  config.access_token        = "XXXXXXXXXXXXXXXXXXX"
-  config.access_token_secret = "XXXXXXXXXXXXXXXXXXX"
+  config.consumer_key        = "lDSxbY9sbXrDmbRS9SBoYrDEF"
+  config.consumer_secret     = "gpPszQsc4DHsU608vNxyzeWIKAoaABhSj6IOkFnPTDYGyEj4J5"
+  config.access_token        = "390711435-T9BeXPtZ6Vwqsjz1tV2AlFuNIWbqy8JJByy7HdGD"
+  config.access_token_secret = "7VaO1cJDq5XcA9HJxlwzbySHVsuI40SBwTaXf47JrTLOt"
 end
+
+
+
+
+# consumer key lDSxbY9sbXrDmbRS9SBoYrDEF
+# consumer secret gpPszQsc4DHsU608vNxyzeWIKAoaABhSj6IOkFnPTDYGyEj4J5
+#
+# Access Token :- 390711435-T9BeXPtZ6Vwqsjz1tV2AlFuNIWbqy8JJByy7HdGD
+#
+# Access Token Secret 7VaO1cJDq5XcA9HJxlwzbySHVsuI40SBwTaXf47JrTLOt
